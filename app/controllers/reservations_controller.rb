0185984f5a64e6ee1c57e5c833f1a135dab9b55a
Rafael Millan <rafmillan@gmail.com>
2017-02-13 18:15:17 +0100

    Add reservation button to place show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,9 @@
 class ReservationsController < ApplicationController
-  before_action :set_place, only: [ :create ]
-
   def create
     @reservation = Reservation.new
     @reservation.user = current_user
-    @reservation.place = @place
-    if @reservation.save
-      redirect_to 'root' # Change once we create a users#show
-    else
-      render "pages/reservation"
-    end
-  end
-
-  private
-
-  def set_place
-    @place = Place.find(params[:id])
+    @reservation.place = Place.find(params[:place_id])
+    @reservation.save
+    redirect_to root_path # Change once we create a users#show
   end
 end
