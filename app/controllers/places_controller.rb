8e92015a6c98842f2b364f74883ed6b384ef075a
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-13 15:28:57 +0100

    Add Controller new and create for places

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    if @place.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address, :price)
+  end
+end
