690d7d663560855622d1c202a47099dd240e58b2
Rafael Millan <rafmillan@gmail.com>
2017-02-14 12:58:20 +0100

    Add photos to seed and to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,14 @@ Faker::UniqueGenerator.clear
   )
 end
 
-30.times do
-  Place.create!(
+20.times do
+  place = Place.new(
     name: Faker::Space.planet,
     description: Faker::Hipster.sentence,
     address: Faker::Address.street_address,
     price: (1000..3000).to_a.sample,
     user: User.all.sample
   )
+  place.photo = Rails.root.join("db/seed_images/#{(1..20).to_a.sample}.jpg").open
+  place.save!
 end
