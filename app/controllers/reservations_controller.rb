264319f5bc19dce804193a45c8eb5e310730a97a
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-15 13:12:55 +0100

    routes fixed

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,7 +7,7 @@ class ReservationsController < ApplicationController
     @reservation.user = current_user
     @reservation.place = Place.find(params[:place_id])
     @reservation.save
-    redirect_to root_path # Change once we create a users#show
+    redirect_to user_path(current_user) # Change once we create a users#show
   end
 
   def accept
