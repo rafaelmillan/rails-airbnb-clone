d81721073cd20d6f1fb2bde527f20e55976aaf1a
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-16 15:05:46 +0100

    ratings added to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  RATING = (1..5)
   belongs_to :place
   validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: RATING, allow_nil: false }, numericality: {only_integer: true }
 end
